fbc7c5d500260a5689f34c122369287f744b4de6
nlgatewood <nlgatewood@gmail.com>
2017-05-06 10:55:40 -0400

    formatting changes

--- cgi-bin/footer.php
+++ cgi-bin/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-	echo "</CENTER>
+	echo "</div>
 			</BODY>
-			</HTML>";
+		 </HTML>";
 ?>
--- cgi-bin/header.php
+++ cgi-bin/header.php
@@ -1,19 +1,6 @@
 <?php
 
 	$page = isset($_GET["pg"]) ? $_GET["pg"] : "";
-	//$fam = isset($_GET["fam"]) ? $_GET["fam"] : "";
-	//$fam_html = "";
-	//$fam_desc = "";
-
-	//Determine the Family Description for the link
-	//if($fam == ""){
-
-	//	$fam_html = "";
-	//	$fam_desc = "Select a Family...";
-	//}else{
-	//	$fam_html = "?fam=$fam";
-	//	$fam_desc = ucfirst($fam);
-	//}
 
 	echo "<HTML>
 			<HEAD>
@@ -27,32 +14,20 @@
                <script language='JavaScript' src='/js/FamilyTree.js'></script>
 				<link href='/css/page_layout.css' rel='stylesheet' type='text/css'/>
 				<link href='/css/family_tree.css' rel='stylesheet' type='text/css'/>
-				<link href='/css/nav_menu.css' rel='stylesheet' type='text/css'/>
 				<link href='/css/lightbox/lightbox.css' rel='stylesheet' type='text/css'/>
 			</HEAD>
 			<BODY>
-			 <CENTER>";
+			 <div id='page-wrapper'>
 
-	echo "<img class='banner' src='/images/tree_of_life_banner.jpg'>
-	      <ul class='navbar'>";
-
-	echo "<li><a class='navbar-link' href='/'>Home</a></li>
-	      <li><a class='navbar-link' href='/?pg=origin'>Family Origins</a></li>
-	      <li><a class='navbar-link' href='/?pg=history'>Family History</a></li>
-	      <li><a class='navbar-link' href='/?pg=tree'>Family Tree</a></li>";
-
-	/*
-   echo "<li><a href='javascript:void(0)' onclick='showDropdownMenu()' class='navbar-link dropbtn'>Family Trees 
-             <img id='fam-menu-arrow' src='/images/down_arrow_12x12.png'></a>
-             <div id='myDropdown' class='dropdown-content'>
-             	<a href='/?pg=family_tree&fam=tj'>Thomas Jefferson to Atwell Bowcock</a>
-               <a href='/?pg=family_tree&fam=jw'>John Wallace Tree</a>
-               <a href='/?pg=family_tree&fam=ca'>Commilus Atwell Tree</a>
-               <a href='/?pg=family_tree&fam=cl'>Cornealus Linzy Tree</a>
-             </div></li>";
-	*/
-
-	echo "	   <li><a class='navbar-link' href='/?pg=about'>About</a></li>
-  		   <li><a class='navbar-link' href='/?pg=contact'>Contact</a></li>
-		  </ul>";
+			  <div id='navbar-wrapper'>
+				<img id='banner' src='/images/tree_of_life_banner.jpg'>
+	      	<ul id='navbar'>
+				 <li><a href='/'>Home</a></li>
+	      	 <li><a href='/?pg=origin'>Family Origins</a></li>
+	      	 <li><a href='/?pg=history'>Family History</a></li>
+	      	 <li><a href='/?pg=tree'>Family Tree</a></li>
+				 <li><a href='/?pg=about'>About</a></li>
+  		   	 <li><a href='/?pg=contact'>Contact</a></li>
+				</ul>
+		 	  </div>";
 ?>
