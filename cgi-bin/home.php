eb248ef9c50a0703faeb652c5758af495a1ec5bf
nlgatewood <nlgatewood@gmail.com>
2017-05-15 14:39:29 -0400

    Update home.php

--- cgi-bin/home.php
+++ cgi-bin/home.php
@@ -12,7 +12,7 @@ echo"<div class='page-text'>
 <h1>Welcome to the Gatewood Family Tree Project!</h1>
 
 <a href='images/family/family_collage.jpg' data-lightbox='image-1' data-title='My caption'>
-<img src='images/family/family_collage.jpg' style='width:700; height:auto; margin-top:30px; margin-bottom:30px;'></a>
+<img src='images/family/family_collage.jpg' style='width:700px; height:auto; margin-top:30px; margin-bottom:30px;'></a>
 
 <p>This Gatewood Ancestry website is currently under construction.  I really hope to have this website up at 90% before the end of Summer 2017.  Most of my information comes from a family book of ancestry written by Gordon Jefferson Gatewood.  While it's a great resource, the book was published in 1981 and needs to be updated.  Many people listed in the book have since died or have children of their own.  
 Also, the author never had the luxury of Google.  He spent years traveling across the country to different courhouses and other facilities gathering information.  There may have been details he missed simply because it wasn't readily available to him.  I'm hoping my research can fill in some of the gaps.  I am in awe of the work he did and the time he put into his research.</p>
